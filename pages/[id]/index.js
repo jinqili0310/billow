0bfafae89f49e52b95f5ee502b4a92743aa8c208
kikijinqili <jinqi.li.310@gmail.com>
2021-02-13 14:58:23 -0800

    posts.index

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -2,7 +2,7 @@
  * @Author: Jinqi Li
  * @Date: 2021-02-13 02:54:09
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2021-02-13 14:48:39
+ * @LastEditTime: 2021-02-13 14:52:41
  * @FilePath: /billow-website/pages/[id]/index.js
  */
 import React, { useState, useEffect } from 'react';
@@ -50,7 +50,7 @@ const Post = ({ post }) => {
 							/>
 						);
 					})}
-					<div dangerouslySetInnerHTML={{ __html: data[post.body] }}></div>
+					<div dangerouslySetInnerHTML={{ __html: post.body }} />
 				</Card>
 			</div>
 		</React.Fragment>
