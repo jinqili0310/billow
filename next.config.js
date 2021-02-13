f5818d0ed40391684ffc892ed9ac2b2b7a2acbbc
kikijinqili <jinqi.li.310@gmail.com>
2021-02-13 12:03:10 -0800

    db name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @Author: Jinqi Li
  * @Date: 2021-02-03 22:18:56
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2021-02-13 09:47:44
+ * @LastEditTime: 2021-02-13 12:02:53
  * @FilePath: /billow-website/next.config.js
  */
 module.exports = {
@@ -45,7 +45,7 @@ module.exports = {
 
 	env: {
 		MONGO_URI:
-			'mongodb+srv://billow-admin:n2UXAYnz3i0OMUyY@clusterbillow.zl3li.mongodb.net/postDB?retryWrites=true&w=majority',
+			'mongodb+srv://billow-admin:n2UXAYnz3i0OMUyY@clusterbillow.zl3li.mongodb.net/billowDB?retryWrites=true&w=majority',
 		CLOUD_API: 'https://v1.nocodeapi.com/jinqili/cloudinary/ihYCGJsCpaNHyPfy',
 		ACCESS_TOKEN_SECRET: 'C@n34$E4fK%J%kQ_Wk9FEn#N&@+XmxPZrSs8sCATQ_YYs*933Y',
 		REFRESH_TOKEN_SECRET: '3==HYx9ZZQQ#v!UYNDKtD!etpk8CnRCS^bnh$gjm7fZ7F-WwNv'
