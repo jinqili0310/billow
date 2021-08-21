f934a5b8e69274a1fd8bca26cfcaf5274b5765f5
kikijinqili <jinqi.li.310@gmail.com>
2021-08-20 19:17:29 -0700

    server error fixed

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@
  * @Author: Jinqi Li
  * @Date: 2021-02-11 16:16:43
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2021-02-11 16:17:18
- * @FilePath: /billow-website/config/index.js
+ * @LastEditTime: 2021-08-20 19:15:35
+ * @FilePath: /billow/config/index.js
  */
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://microbillow.com';
+export const server = dev ? 'http://localhost:3000' : 'https://billow-gilt.vercel.app';
